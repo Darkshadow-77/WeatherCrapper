eb4cadc0482a82c99af44c2c4a075645b0a7299c
darkshadow-77 <darkshadow90711@gmail.com>
2025-06-29 23:36:44 +0000

    Animator updated

--- app/src/main/java/com/vandal/weathercrapper/panelAnimator.java
+++ app/src/main/java/com/vandal/weathercrapper/panelAnimator.java
@@ -1,4 +1,3 @@
-
 package com.vandal.weathercrapper;
 
 import android.view.View;
@@ -6,29 +5,41 @@ import android.view.animation.TranslateAnimation;
 
 public class PanelAnimator {
 
+    //  Fait apparaître la vue en glissant du bas vers le haut
     public static void slideInFromBottom(View view, long duration) {
-        view.setVisibility(View.VISIBLE);
-        TranslateAnimation animation = new TranslateAnimation(
-            0, 0, view.getHeight(), 0 // From bottom to current Y
-        );
-        animation.setDuration(duration);
-        animation.setFillAfter(true);
-        view.startAnimation(animation);
+        view.post(() -> {
+            int height = view.getHeight();
+            view.setVisibility(View.VISIBLE);
+            view.clearAnimation(); // Empêche les conflits d'animations
+            TranslateAnimation animation = new TranslateAnimation(
+                0, 0, height, 0
+            );
+            animation.setDuration(duration);
+            animation.setFillAfter(true);
+            view.startAnimation(animation);
+        });
     }
 
+    // Fait disparaître la vue en glissant du haut vers le bas
     public static void slideOutToBottom(View view, long duration) {
-        TranslateAnimation animation = new TranslateAnimation(
-            0, 0, 0, view.getHeight() // From current Y to bottom
-        );
-        animation.setDuration(duration);
-        animation.setFillAfter(true);
-        view.startAnimation(animation);
-        animation.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
-            @Override public void onAnimationEnd(android.view.animation.Animation animation) {
-                view.setVisibility(View.GONE);
-            }
-            @Override public void onAnimationStart(android.view.animation.Animation animation) {}
-            @Override public void onAnimationRepeat(android.view.animation.Animation animation) {}
+        view.post(() -> {
+            int height = view.getHeight();
+            view.clearAnimation();
+            TranslateAnimation animation = new TranslateAnimation(
+                0, 0, 0, height
+            );
+            animation.setDuration(duration);
+            animation.setFillAfter(true);
+            animation.setAnimationListener(new android.view.animation.Animation.AnimationListener() {
+                @Override public void onAnimationStart(android.view.animation.Animation animation) {}
+                @Override public void onAnimationRepeat(android.view.animation.Animation animation) {}
+
+                @Override
+                public void onAnimationEnd(android.view.animation.Animation animation) {
+                    view.setVisibility(View.GONE);
+                }
+            });
+            view.startAnimation(animation);
         });
     }
 }
